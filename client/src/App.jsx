f0a189e048304465d25ba9219690d27e6fd7cc59
Alhaanahmed-duet <Alhaanahmed123@gmail.com>
2024-02-15 14:01:15 +0500

    Main page added, only pages left

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
 import Navbar from './components/common/Navbar/main'
+import Main from './components/features/Main/main'
 function App() {
   return (
     <>
       <Navbar />
+      <Main />
     </>
   )
 }
--- client/src/components/common/Navbar/main.jsx
+++ client/src/components/common/Navbar/main.jsx
@@ -6,12 +6,13 @@ function Navbar() {
         <header>
             <nav>
                 <div className={styles.container}>
-                    <div className={styles.logo}>Quizify</div>
+                    <div className={styles.logo}><i className="bi bi-backpack2-fill"></i> Quizify</div>
                     <ul>
                         <li><a href="#">About</a></li>
                         <li><a href="#">Get Help?</a></li>
                         <li><a href="#">Sign-Up</a></li>
                     </ul>
+                    <button id={styles.btn}><i className="bi bi-list-nested"></i></button>
                 </div>
             </nav>
         </header>
