ce44c6f5ba2560241114e0cc034b4d06fbd11393
Alhaanahmed-duet <Alhaanahmed123@gmail.com>
2024-02-19 16:51:24 +0500

    fixed form but styling left

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -7,18 +7,25 @@ const SignUp = () => {
       <Navbar />
       <form onSubmit={(event) => event.preventDefault()}>
             <label htmlFor='username' >Enter your name</label>
+            <br />
             <input name='username' type='text' placeholder='username' />
             <br />
             <label htmlFor='email'>Enter your email address</label>
+            <br />
             <input name='email' type='email' placeholder='name@example.com' />
             <br />
             <label htmlFor='password'>Enter your password</label>
+            <br />
             <input name='password' type='text' placeholder='**********' />
             <br />
             <label htmlFor='role'>Your Role</label>
-            <input type='radio' value="Student"/> 
-            <input type='radio' value="Teacher"/>
-            <input type='radio' value="Both of them"/>
+            <br />
+            <input type='radio' name="role" value="Student"/> Student 
+            <br />
+            <input type='radio' name="role" value="Teacher"/> Teacher
+            <br />
+            <input type='radio' name="role" value="Both"/> Both of them
+            <br />
             <button type='submit'>Register</button>
       </form>
 
