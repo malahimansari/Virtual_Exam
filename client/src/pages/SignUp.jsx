9bfa56444192db577dc80649fcae4c4de37bb0b1
Alhaanahmed-duet <Alhaanahmed123@gmail.com>
2024-02-23 02:54:01 +0500

    backend added

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,33 +1,30 @@
 import React from 'react';
 import Navbar from '../components/common/Navbar/main';
-
+import styles from '../styles/signup.module.css';
 const SignUp = () => {
+  
   return (
     <>
       <Navbar />
-      <form onSubmit={(event) => event.preventDefault()}>
-            <label htmlFor='username' >Enter your name</label>
+      <div className={styles.container}>
+      <form id={styles.form} onSubmit={(event) => event.preventDefault()}>
+        <h1>Create your account <i class="bi bi-pencil-square"></i></h1>
+            <label htmlFor='username' >User name</label>
             <br />
             <input name='username' type='text' placeholder='username' />
             <br />
-            <label htmlFor='email'>Enter your email address</label>
+            <label htmlFor='email'>Email address</label>
             <br />
             <input name='email' type='email' placeholder='name@example.com' />
             <br />
-            <label htmlFor='password'>Enter your password</label>
+            <label htmlFor='password'>Password</label>
             <br />
-            <input name='password' type='text' placeholder='**********' />
+            <input name='password' type='text' placeholder='password' />
             <br />
-            <label htmlFor='role'>Your Role</label>
-            <br />
-            <input type='radio' name="role" value="Student"/> Student 
-            <br />
-            <input type='radio' name="role" value="Teacher"/> Teacher
-            <br />
-            <input type='radio' name="role" value="Both"/> Both of them
-            <br />
-            <button type='submit'>Register</button>
+            <button className={styles.btnSubmit} type="submit">Register <i class="bi bi-box-arrow-in-right"></i></button>
       </form>
+      </div>
+     
 
     </>
   )
