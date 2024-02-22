a943c025f90dcf1f936b8cb9f6a45dd9edfdb3e6
AwaisRehman262 <aw2946652@gmail.com>
2024-02-22 14:36:51 +0500

    login and signup backend coonnected 1

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React from "react";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [user, setUser] = useState({
+    username: "",
+    email: "",
+    password: "",
+    role: "",
+  });
 
-export default Login
+  const inputHandler = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+    console.log(user);
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch("127.0.0.1:262/api/auth/signIn", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    });
+    console.log(response);
+  };
+  return (
+    <>
+      <Navbar />
+      <form onSubmit={(event) => handleSubmit}>
+        <label htmlFor="email">Enter your email address</label>
+        <br />
+        <input
+          name="email"
+          type="email"
+          placeholder="name@example.com"
+          onChange={inputHandler}
+        />
+        <br />
+        <label htmlFor="password">Enter your password</label>
+        <br />
+        <input
+          name="password"
+          type="text"
+          placeholder="**********"
+          onChange={inputHandler}
+        />
+        <br />
+
+        <button type="submit">Register</button>
+      </form>
+    </>
+  );
+};
+
+export default Login;
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,36 +1,90 @@
-import React from 'react';
-import Navbar from '../components/common/Navbar/main';
+import React, { useState } from "react";
+import Navbar from "../components/common/Navbar/main";
 
 const SignUp = () => {
+  const [user, setUser] = useState({
+    username: "",
+    email: "",
+    password: "",
+    role: "",
+  });
+
+  const inputHandler = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+    console.log(user);
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch("127.0.0.1:262/api/auth/signUp", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    });
+    console.log(response);
+  };
   return (
     <>
       <Navbar />
-      <form onSubmit={(event) => event.preventDefault()}>
-            <label htmlFor='username' >Enter your name</label>
-            <br />
-            <input name='username' type='text' placeholder='username' />
-            <br />
-            <label htmlFor='email'>Enter your email address</label>
-            <br />
-            <input name='email' type='email' placeholder='name@example.com' />
-            <br />
-            <label htmlFor='password'>Enter your password</label>
-            <br />
-            <input name='password' type='text' placeholder='**********' />
-            <br />
-            <label htmlFor='role'>Your Role</label>
-            <br />
-            <input type='radio' name="role" value="Student"/> Student 
-            <br />
-            <input type='radio' name="role" value="Teacher"/> Teacher
-            <br />
-            <input type='radio' name="role" value="Both"/> Both of them
-            <br />
-            <button type='submit'>Register</button>
+      <form onSubmit={(event) => handleSubmit}>
+        <label htmlFor="username">Enter your name</label>
+        <br />
+        <input
+          name="username"
+          type="text"
+          placeholder="username"
+          onChange={inputHandler}
+        />
+        <br />
+        <label htmlFor="email">Enter your email address</label>
+        <br />
+        <input
+          name="email"
+          type="email"
+          placeholder="name@example.com"
+          onChange={inputHandler}
+        />
+        <br />
+        <label htmlFor="password">Enter your password</label>
+        <br />
+        <input
+          name="password"
+          type="text"
+          placeholder="**********"
+          onChange={inputHandler}
+        />
+        <br />
+        <label htmlFor="role">Your Role</label>
+        <br />
+        <input
+          type="radio"
+          name="role"
+          value="Student"
+          onChange={inputHandler}
+        />{" "}
+        Student
+        <br />
+        <input
+          type="radio"
+          name="role"
+          value="Teacher"
+          onChange={inputHandler}
+        />{" "}
+        Teacher
+        <br />
+        <input
+          type="radio"
+          name="role"
+          value="Both"
+          onChange={inputHandler}
+        />{" "}
+        Both of them
+        <br />
+        <button type="submit">Register</button>
       </form>
-
     </>
-  )
-}
+  );
+};
 
 export default SignUp;
