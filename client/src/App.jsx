048e65bd93bbd9d1afed6c5a852d5130a211fc13
AwaisRehman262 <aw2946652@gmail.com>
2024-03-03 13:06:24 +0500

    created awaisnewbranch

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,22 +10,21 @@ import Dashboard from "./pages/Dashboard";
 import { FormProvider } from "./context/FormProvider";
 import Quiz from "./pages/Quiz";
 
-
 function App() {
   return (
     <FormProvider>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Main />}></Route>
-        <Route path="/About" element={<About />}></Route>
-        <Route path="/LogIn" element={<Login />}></Route>
-        <Route path="/SignUp" element={<SignUp />}></Route>
-        <Route path="/logOut" element={<LogOut />}></Route>
-        <Route path="/createtest" element={<CreateTest />} />
-        <Route path='/dashboard' element={<Dashboard />}></Route>
-        <Route path='/quiz' element={<Quiz />}></Route>
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main />}></Route>
+          <Route path="/About" element={<About />}></Route>
+          <Route path="/LogIn" element={<Login />}></Route>
+          <Route path="/SignUp" element={<SignUp />}></Route>
+          <Route path="/logOut" element={<LogOut />}></Route>
+          <Route path="/createtest" element={<CreateTest />} />
+          <Route path="/dashboard" element={<Dashboard />}></Route>
+          <Route path="/quiz" element={<Quiz />}></Route>
+        </Routes>
+      </BrowserRouter>
     </FormProvider>
   );
 }
